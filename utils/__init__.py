fa1b97e00ae45f1e7ad84b7210a09d3922906761
ANDREW HIGGINS <AJHIGGINS96@BERKELEY.EDU>
2024-09-03 11:28:16 -0400

    Replace dead Github image

--- utils/__init__.py
+++ utils/__init__.py
@@ -26,7 +26,7 @@ def sidebar() -> None:
             st.html('<a href="mailto:ajhiggins96@gmail.com">ajhiggins96@gmail.com</a>')
         row1 = st.columns([0.1, 0.9])
         with row1[0]:
-            st.image('https://camo.githubusercontent.com/dfe7e80288901f8d5e8de7562d6f94491e2a7f8042316fd544fe3b6364b63783/68747470733a2f2f69636f6e2d6c6962726172792e636f6d2f696d616765732f6769746875622d69636f6e2d77686974652f6769746875622d69636f6e2d77686974652d362e6a7067', width=25)
+            st.image('static/image/github-mark-white.png', width=25)
         with row1[1]:
             st.link_button(label='website source', url='https://github.com/ajhiggins96/Portfolio-Site/tree/main')
 
